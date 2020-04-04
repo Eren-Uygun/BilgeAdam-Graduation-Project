86bd6359565c296fab250c2711a3a371ee565f91
Eren Uyğun <eren.uygun.2657@gmail.com>
2020-04-04 14:21:07 +0300

    Image and logger try

--- PharmaceuticalWarehouseManagementSystem/PharmaceuticalWarehouseManagementSystem.DAL/Context/ProjectContext.cs
+++ PharmaceuticalWarehouseManagementSystem/PharmaceuticalWarehouseManagementSystem.DAL/Context/ProjectContext.cs
@@ -34,6 +34,38 @@ namespace PharmaceuticalWarehouseManagementSystem.DAL.Context
             modelBuilder.ApplyConfiguration(new ProductMap());
             modelBuilder.ApplyConfiguration(new ShipperMap());
             modelBuilder.ApplyConfiguration(new SupplierMap());
+
+
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee()
+                {
+                    ID = new Guid(), FirstName = "Admin", LastName = "Admin", Role = Role.Admin,
+                    Email = "admin@mail.com", Password = "123",BirthDate = DateTime.Parse("10/11/2020 17:00"),HireDate = DateTime.Parse("24/02/2020 09:05")
+                },
+
+            new Employee()
+                {
+                    ID = new Guid(), FirstName = "User", LastName = "User", Role = Role.User,
+                    Email = "user@mail.com", Password = "123",BirthDate = DateTime.Parse("10/11/2020 17:00"),HireDate = DateTime.Parse("24/02/2020 09:05")
+                }
+
+            );
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category()
+                {
+                    ID = new Guid(), CategoryName = "Ağrı kesici",
+                    CategoryDescription = "Düşük ve orta düzey ağrı giderici ilaçlar"
+                },
+
+                new Category()
+                    {
+                        ID = new Guid(),CategoryName = "Ateş Düşürücü",CategoryDescription = "Vücut sıcaklığını ayarlamaya yarayan ilaçlar"
+                    }
+
+            );
+
+
             
 
 
